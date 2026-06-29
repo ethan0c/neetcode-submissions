5516193a2b70e7cf6d77e67a771bfdb54cac1930
Chibudom Ethan Onyejesi <123244280+ethan0c@users.noreply.github.com>
2026-06-29 09:11:13 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+age = 25
+name = "Alice"
+my_list = [1,2,3]
+is_true = True
+temperature = 98.6
+
+
+
+
+print(type(10))
+print(type(5.4))
+print(type(True))
+print(type("string_type"))
+print(type([1]))
